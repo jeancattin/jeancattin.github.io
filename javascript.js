d994b8f3c2d7e1e20b730b121601333d6a0f4b70
jeancattin <jean.cattin@protonmail.ch>
2021-08-22 00:28:40 +0200

    Move to github pages

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 var quotes = [
-  "L'alignement de Neptune et Vénus avec le champ magnétique terrestre signifie une période propice pour détoxifier votre foie. \n(Martin Millon)",
+  "L'alignement de Neptune et Vénus avec le champ magnétique terrestre signifie une période propice pour détoxifier votre foie.",
   "Cet espace silencieux entre vos pensées est votre fenêtre sur l'esprit cosmique. (Deepak Chopra)",
   "Votre corps forme un tout indissociable avec l'univers. Quand il est en parfaite santé et intact, vous vous sentez en expansion. (Deepak Chopra)",
-  "Ta gueule, c'est quantique! (Martin Millon)"
+  "Ta gueule, c'est quantique! ",
+  "Si, en regardant le ciel, vous voyez quatre étoiles formant un carré, c'est qu'il est l'heure de se taire.",
+  "Le téléscope n'est qu'un outil servant à projeter la conscience au travers du temps et de l'espace."
 ]
 
 function newQuote() {
