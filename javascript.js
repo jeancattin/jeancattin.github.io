04d6560cb7e3f1e360a40eda8aae5449d1c5328c
jeancattin <jean.cattin@protonmail.ch>
2021-08-26 22:33:20 +0200

    Ajout phrases + biblio

--- javascript.js
+++ javascript.js
@@ -1,10 +1,14 @@
 var quotes = [
-  "L'alignement de Neptune et Vénus avec le champ magnétique terrestre signifie une période propice pour détoxifier votre foie.",
-  "Cet espace silencieux entre vos pensées est votre fenêtre sur l'esprit cosmique. (Deepak Chopra)",
-  "Votre corps forme un tout indissociable avec l'univers. Quand il est en parfaite santé et intact, vous vous sentez en expansion. (Deepak Chopra)",
-  "Ta gueule, c'est quantique! ",
-  "Si, en regardant le ciel, vous voyez quatre étoiles formant un carré, c'est qu'il est l'heure de se taire.",
-  "Le téléscope n'est qu'un outil servant à projeter la conscience au travers du temps et de l'espace."
+  "La thèse donne un sacré bonus!",
+  "Lorsque les planètes sont nettes, il faut un plus gros téléscope.",
+  "Un message spatial procure tant de bonheur!",
+  "Quant un pulsar dégage l'orbite, mieux vaut s'éloigner!",
+  "En concentrant son troisième oeil, on peut provoquer l'annulation d'une nova.",
+  "L'effet des forces est avant tout une question de pression."
+  "Lorsque vous êtes tenté par un bel amas, n'hésitez pas à braquer votre téléscope!"
+  "La quote de bien des comètes forment des courbes harmonieuses."
+  "La théorie des super cubes m'a toujours semblé évidente."
+  "On a pas besoin d'une grosse auto pour observer l'écliptique."
 ]
 
 function newQuote() {
