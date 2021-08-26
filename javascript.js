fd57708f5c46005ba09df81bb656117db525f18c
jeancattin <jean.cattin@protonmail.ch>
2021-08-26 22:40:57 +0200

    Correction bug phrases

--- javascript.js
+++ javascript.js
@@ -4,10 +4,10 @@ var quotes = [
   "Un message spatial procure tant de bonheur!",
   "Quant un pulsar dégage l'orbite, mieux vaut s'éloigner!",
   "En concentrant son troisième oeil, on peut provoquer l'annulation d'une nova.",
-  "L'effet des forces est avant tout une question de pression."
-  "Lorsque vous êtes tenté par un bel amas, n'hésitez pas à braquer votre téléscope!"
-  "La quote de bien des comètes forment des courbes harmonieuses."
-  "La théorie des super cubes m'a toujours semblé évidente."
+  "L'effet des forces est avant tout une question de pression.",
+  "Lorsque vous êtes tenté par un bel amas, n'hésitez pas à braquer votre téléscope!",
+  "La quote de bien des comètes forment des courbes harmonieuses.",
+  "La théorie des super cubes m'a toujours semblé évidente.",
   "On a pas besoin d'une grosse auto pour observer l'écliptique."
 ]
 
