de3dd9fb883779cd2385bdcee2dac02162d25c0a
jeancattin <jean.cattin@protonmail.ch>
2021-08-28 00:15:16 +0200

    Ajout phrases

--- javascript.js
+++ javascript.js
@@ -8,7 +8,24 @@ var quotes = [
   "Lorsque vous êtes tenté par un bel amas, n'hésitez pas à braquer votre téléscope!",
   "Durant ma carrière, j'ai obtenu la quote de bien des comètes.",
   "La théorie des super cubes m'a toujours semblé évidente.",
-  "On a pas besoin d'une grosse auto pour observer l'écliptique."
+  "On a pas besoin d'une grosse auto pour observer l'écliptique.",
+  "La méditation transcendentale ouvre l'accès au monde conique.",
+  "Mes collègues et moi aimons contester nos particules.",
+  "À bout de course, les objets stellaires finissent par s'arrêter.",
+  "À force de butter, les hommes verront leurs mythes disparaître.",
+  "La connaissance de la métaphysique couvre les Suisses.",
+  "Pour observer le cosmos à la belle saison, les nuits paisibles posent problème.",
+  "Aux Alliés, le bois va jusqu'au Doubs.",
+  "J'ai toujours aimé la médecine naturelle. Si vous saviez ce que votre plante me fait...",
+  "Les preuves sont sous les voutes de l'univers.",
+  "Entouré d'incapables, il est difficile de distinguer l'écho des sons.",
+  "Après une grande découverte, faut-il faire le malin dans la cour ?",
+  "Devant la taille de l'univers, on se sent abaissé par tant de grandeur.",
+  "Dans un amas de nébuleuses, les shorts sont mauves.",
+  "En radioastronomie, le son passe tellement vite qu'on risque de se briser la nuque.",
+  "La plupart des gens se laissent abuser par les mythes de la science."
+  
+  "Ce vaccin, quel sujet! Ne croy"
 ]
 
 function newQuote() {
